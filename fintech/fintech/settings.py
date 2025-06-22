3cb666da466bf247cb6aa967e54ce19c6387feb2
Augustine1508 <adozienwa@gmail.com>
2025-06-22 20:06:54 +0100

    turned off debug mode

--- fintech/fintech/settings.py
+++ fintech/fintech/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*=eegocajak16u8^0-_c#%*g57wgr2c*z+nb-wsx5-umn9$iy-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dauxen-bank.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
